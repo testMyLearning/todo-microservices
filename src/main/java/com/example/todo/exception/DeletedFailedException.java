4ec276fadbcd7bff5881c8e96955f676e2b6b276
mqwerq <swalka1995@yandex.ru>
2026-02-17 23:30:10 +0500

    ошибка с апи модулем

--- src/main/java/com/example/todo/exception/DeletedFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.todo.exception;
-
-public class DeletedFailedException extends RuntimeException {
-    public DeletedFailedException(String email){
-        super("Не удалось удалить пользователя с таким email "+email);
-    }
-}
--- src/main/java/com/example/todo/model/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.todo.model.enums;
-
-public enum Role {
-    USER,
-    ADMIN;
-}
