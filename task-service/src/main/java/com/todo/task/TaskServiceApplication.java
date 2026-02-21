2d1daa8d3d4064aacdbfa9f6acacf9020d581b7e
mqwerq <swalka1995@yandex.ru>
2026-02-21 20:00:32 +0500

    все мониторы работают

--- task-service/src/main/java/com/todo/task/TaskServiceApplication.java
+++ task-service/src/main/java/com/todo/task/TaskServiceApplication.java
@@ -2,13 +2,12 @@ package com.todo.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication(scanBasePackages = {
         "com.todo.task",  // сканирует твой код
         "com.todo.common"    // сканирует код из common модуля!
 })
-@EnableFeignClients
 public class TaskServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskServiceApplication.class, args);
